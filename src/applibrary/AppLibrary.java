42f2b65e0c1503b657a9dd141117cd0eaa561361
r4scodee <irbadhganzz@gmail.com>
2025-08-21 14:16:03 +0700

    Project java JFrameForm

---
+++ src/applibrary/AppLibrary.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package applibrary;
+
+/**
+ *
+ * @author Software-19
+ */
+public class AppLibrary {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
